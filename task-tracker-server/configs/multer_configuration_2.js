337a9c6b4250c3b4bfea8f46a60d9a8579312caf
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-01-22 23:22:43 -0500

    Reverted previous commit changes and stored them in a separate file

--- task-tracker-server/configs/multer_configuration_2.js
+++ task-tracker-server/configs/multer_configuration_2.js
@@ -1,16 +1,13 @@
-// Copyright (c) 2020 Anastasiia Birillo
+// JavaScript
+const path = require('path');
+const crypto = require('crypto');
 
 module.exports = (multer) => {
-
-    const crypto = require('crypto');
-
     const storage = multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, `${__dirname}/../uploads/`)
+            cb(null, `${__dirname}/../uploads/`);
         },
-
         filename: function (req, file, cb) {
-            const path = require('path');
             const currentDate = Date.now();
             const min = 0;
             const randomNumber = Math.random() * (currentDate - min) + min;
@@ -18,8 +15,10 @@ module.exports = (multer) => {
             const hashedName = file.originalname.split(' ').join('_').split('.').slice(0, -1).join('.') + '_'
                 + randomNumber + '_' +
                 crypto.createHmac('sha1', crypto.createHmac('sha256', '0').update(file.mimetype).digest('hex').toString())
-                    .update(filename).digest('hex');
-            cb(null, hashedName + path.extname(file.originalname));
+                .update(filename).digest('hex');
+            const relativePath = `uploads/${hashedName + path.extname(file.originalname)}`;
+            const formattedPath = `http://localhost:3000/${relativePath.replace(/\\/g, '/')}`;
+            cb(null, formattedPath);
         }
     });
 
