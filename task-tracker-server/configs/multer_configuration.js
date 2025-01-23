009da3ace006a0516432bc17a1a7e9974aacf1d4
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-01-22 22:36:41 -0500

    Fixed minor bug causing GET request to crash, users collection isn't generating

--- task-tracker-server/configs/multer_configuration.js
+++ task-tracker-server/configs/multer_configuration.js
@@ -1,16 +1,13 @@
-// Copyright (c) 2020 Anastasiia Birillo
+// JavaScript
+const path = require('path');
+const crypto = require('crypto');
 
 module.exports = (multer) => {
-
-    const crypto = require('crypto');
-
     const storage = multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, `${__dirname}/../uploads/`)
+            cb(null, `${__dirname}/../uploads/`);
         },
-
         filename: function (req, file, cb) {
-            const path = require('path');
             const currentDate = Date.now();
             const min = 0;
             const randomNumber = Math.random() * (currentDate - min) + min;
@@ -19,7 +16,9 @@ module.exports = (multer) => {
                 + randomNumber + '_' +
                 crypto.createHmac('sha1', crypto.createHmac('sha256', '0').update(file.mimetype).digest('hex').toString())
                 .update(filename).digest('hex');
-            cb(null, hashedName + path.extname(file.originalname));
+            const relativePath = `uploads/${hashedName + path.extname(file.originalname)}`;
+            const formattedPath = `http://localhost:3000/${relativePath.replace(/\\/g, '/')}`;
+            cb(null, formattedPath);
         }
     });
 
