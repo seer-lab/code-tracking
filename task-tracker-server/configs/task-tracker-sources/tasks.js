fafdc54eb6392f05c75abfff2bbd428488355fb2
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-01-24 19:09:58 -0500

    Added tasks to the server, but the interface will need some alterations later

--- task-tracker-server/configs/task-tracker-sources/tasks.js
+++ task-tracker-server/configs/task-tracker-sources/tasks.js
@@ -1,210 +1,114 @@
 const tasks = [
     {
-        'key': 'pies',
+        'key': 'Statement Coverage',
         'examples':
             [
                 {
-                    'input': '10\n' +
-                        '15\n' +
-                        '2',
-                    'output': '20 30'
+                    'input': 'TBD',
+                    'output': 'TBD'
                 },
-                {
-                    'input': '2\n' +
-                        '50\n' +
-                        '4',
-                    'output': '10 0'
-                },
-                {
-                    'input': '2\n' +
-                        '50\n' +
-                        '0',
-                    'output': '0 0'
-                }
             ],
         'descriptions':
             [
                 {
                     'language': 'en',
                     'info': {
-                        'name': 'Pies',
-                        'description': 'A single pie costs A dollars and B cents in the cafe. Calculate how many dollars and cents you would need to buy N pies.',
-                        'input': 'The program receives three numbers as an input:\n' +
-                            'A - how many dollars a pie costs;\n' +
-                            'B - how many cents a pie costs;\n' +
-                            'N - how many pies do you need to buy.',
-                        'output': 'Print out two numbers: the cost of N pies in dollars and cents.'
+                        'name': 'Statement Coverage',
+                        'description': 'Causes every statement in the program to be executed at least once, ' +
+                            'giving us confidence that every statement is at least capable of executing correctly.\n\n' +
+                            'System: Make a test case for each statement in the program, independent of the others.\n\n' +
+                            'Completion criterion: A test case for every statement.\n\n' +
+                            'Note: You only need to do statement coverage for the provided code for this study.',
+                        'input': 'TBD',
+                        'output': 'TBD'
                     }
                 }
             ]
     },
     {
-        'key': 'max_3',
+        'key': 'Branch Coverage',
         'examples':
             [
                 {
-                    'input': '1\n' +
-                        '2\n' +
-                        '3',
-                    'output': '3'
+                    'input': 'TBD',
+                    'output': 'TBD'
                 },
-                {
-                    'input': '5\n' +
-                        '5\n' +
-                        '5',
-                    'output': '5'
-                },
-                {
-                    'input': '2\n' +
-                        '1\n' +
-                        '1',
-                    'output': '1'
-                }
             ],
         'descriptions':
             [
                 {
                     'language': 'en',
                     'info': {
-                        'name': 'Max 3',
-                        'description': 'Print the largest of three numbers in the input.',
-                        'input': 'The program receives three numbers as an input',
-                        'output': 'Print the largest number. If there are several largest among the three numbers, print only one of them.'
+                        'name': 'Branch Coverage',
+                        'description': 'Causes every decision (if, switch, while, etc.) in the program to be made both ways ' +
+                            '(or every possible way for switch).\n\n' +
+                            'System: Design a test case to exercise each decision in the program each way (true/false).\n\n' +
+                            'Completion criterion: A test case for each side of each decision.\n\n' +
+                            'Note: You only need to do branch coverage for the provided code for this study.',
+                        'input': 'TBD',
+                        'output': 'TBD'
                     }
                 }
             ]
     },
     {
-        'key': 'is_zero',
+        'key': 'Path Coverage',
         'examples':
             [
                 {
-                    'input': '3\n' +
-                        '0\n' +
-                        '1',
-                    'output': 'YES'
+                    'input': 'TBD',
+                    'output': 'TBD'
                 },
-                {
-                    'input': '3\n' +
-                        '-15\n' +
-                        '1',
-                    'output': 'NO'
-                },
-                {
-                    'input': '2\n' +
-                        '0\n' +
-                        '0',
-                    'output': 'YES'
-                }
             ],
         'descriptions':
             [
                 {
                     'language': 'en',
                     'info': {
-                        'name': 'Is zero',
-                        'description': 'Check if there are zeros among numbers in the input.',
-                        'input': 'The program receives several numbers as an input:\n' +
-                            'N - how many numbers do you need to input;\n' +
-                            'N numbers to check.',
-                        'output': 'Print YES if there are zeros among numbers in the input and NO otherwise. You must enter all N numbers and only after that the inscription YES or NO should appear.'
+                        'name': 'Path Coverage',
+                        "description": "Ensures that every unique path through the program, from start to finish, is executed at least once.\n" +
+                            "System: Design test cases to cover all possible paths in the control flow graph of the program.\n" +
+                            "Completion criterion: A test case for every distinct control flow path.\n" +
+                            "Note: You only need to do path coverage for the provided code for this study.",
+                        'input': 'TBD',
+                        'output': 'TBD'
                     }
                 }
             ]
     },
-    {
-        'key': 'max_digit',
-        'examples':
-            [
-                {
-                    'input': '11111111',
-                    'output': '1'
-                },
-                {
-                    'input': '123034655943',
-                    'output': '9'
-                },
-                {
-                    'input': '4',
-                    'output': '4'
-                }
-            ],
-        'descriptions':
-            [
-                {
-                    'language': 'en',
-                    'info': {
-                        'name': 'Max digit',
-                        'description': 'Given a string containing only digits, find and print the largest digit.',
-                        'input': 'The program receives a string containing only digits.',
-                        'output': 'Print the largest digit that occurs more often in the string.'
-                    }
-                }
-            ]
-    },
-    {
-        'key': 'voting',
-        'examples':
-            [
-                {
-                    'input': '0 0 1',
-                    'output': '0'
-                },
-                {
-                    'input': '1 1 1',
-                    'output': '1'
-                },
-                {
-                    'input': '1 0 1',
-                    'output': '1'
-                }
-            ],
-        'descriptions':
-            [
-                {
-                    'language': 'en',
-                    'info': {
-                        'name': 'Voting',
-                        'description': 'Given three numbers, each of them being 1 or 0, determine which one occurs more often — 1 or 0.',
-                        'input': 'The program receives three numbers as an input, each being 1 or 0.',
-                        'output': 'Print out the number that occurs more often.'
-                    }
-                }
-            ]
-    },
-    {
-        'key': 'brackets',
-        'examples':
-            [
-                {
-                    'input': 'example',
-                    'output': 'e(x(a(m)p)l)e'
-                },
-                {
-                    'input': 'card',
-                    'output': 'c(ar)d'
-                },
-                {
-                    'input': 'ab',
-                    'output': 'ab'
-                }
-            ],
-        'descriptions':
-            [
-                {
-                    'language': 'en',
-                    'info': {
-                        'name': 'Brackets',
-                        'description': 'Place the opening and the closing brackets into the input string like this:\nfor odd length\n' +
-                            'example → e(x(a(m)p)l)e,\n' +
-                            'for even length\n' +
-                            'card → c(ar)d, but not c(a()r)d.',
-                        'input': 'The program receives a string of English letters (lowercase and uppercase) as an input.',
-                        'output': 'Print out the string with the brackets added.'
-                    }
-                }
-            ]
-    }
+    // {
+    //     'key': 'brackets',
+    //     'examples':
+    //         [
+    //             {
+    //                 'input': 'example',
+    //                 'output': 'e(x(a(m)p)l)e'
+    //             },
+    //             {
+    //                 'input': 'card',
+    //                 'output': 'c(ar)d'
+    //             },
+    //             {
+    //                 'input': 'ab',
+    //                 'output': 'ab'
+    //             }
+    //         ],
+    //     'descriptions':
+    //         [
+    //             {
+    //                 'language': 'en',
+    //                 'info': {
+    //                     'name': 'Brackets',
+    //                     'description': 'Place the opening and the closing brackets into the input string like this:\nfor odd length\n' +
+    //                         'example → e(x(a(m)p)l)e,\n' +
+    //                         'for even length\n' +
+    //                         'card → c(ar)d, but not c(a()r)d.',
+    //                     'input': 'The program receives a string of English letters (lowercase and uppercase) as an input.',
+    //                     'output': 'Print out the string with the brackets added.'
+    //                 }
+    //             }
+    //         ]
+    // }
 ];
 
 module.exports = tasks;
