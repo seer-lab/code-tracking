b18cedabf571d97a7ce9b7e498c3ea6555f4cc9f
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2025-02-26 13:12:00 -0500

    Corrected the version of PyCharm to 2024.3.3

--- task-tracker-plugin/build.gradle.kts
+++ task-tracker-plugin/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     testImplementation("junit:junit:4.12")
 
     intellijPlatform {
-        val ideVersion = System.getenv().getOrDefault("TASK_TRACKER_PYCHARM_VERSION", "2024.3.1")
+        val ideVersion = System.getenv().getOrDefault("TASK_TRACKER_PYCHARM_VERSION", "2024.3.3")
         println("Using ide version: $ideVersion")
         create("PY", ideVersion) // 'PY' for PyCharm Professional, 'PC' for PyCharm Community
         instrumentationTools()
